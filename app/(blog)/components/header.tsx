339134d5e20355d23d9235f31670c8eee975ff70
Bay Phillips <bay.phillips@gmail.com>
2024-10-04 19:46:48 -0400

    header back to root

--- app/(blog)/components/header.tsx
+++ app/(blog)/components/header.tsx
@@ -1,6 +1,7 @@
 import { SettingsQueryResult } from "@/sanity.types";
 import Navigation from "./navigation";
 import PortableText from "./portable-text";
+import Link from "next/link";
 
 function Intro(props: { title: string | null | undefined; description: any }) {
   const title = props.title ;
@@ -8,9 +9,11 @@ function Intro(props: { title: string | null | undefined; description: any }) {
 
   return (
     <section className="mt-16 mb-8 md:mb-16 flex flex-col items-left lg:mb-12 lg:flex-column lg:justify-between">
-      <h2 className="font-serif text-balance text-6xl font-bold leading-tight tracking-tighter lg:text-8xl">
-        {title}
-      </h2>
+      <Link href={'/'}>
+        <h2 className="font-serif text-balance text-6xl font-bold leading-tight tracking-tighter lg:text-8xl">
+          {title}
+        </h2>
+      </Link>
       <h3 className="text-pretty mt-5 text-left text-lg">
         <PortableText
           className="prose-lg"
